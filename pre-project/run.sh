afdab4eb07e5d233615f21fa3561113488ae74ed
Diogo Cezar Teixeira Batista <diogo.batista@meifacil.com>
2020-05-30 11:56:13 -0300

    chore: adjustments pre-project

--- pre-project/run.sh
+++ pre-project/run.sh
@@ -2,7 +2,3 @@
 #find pre-project.* -type f -not -name 'pre-project.tex' -not -name 'pre-project.bib' -delete
 pdflatex pre-project
 bibtex pre-project
-pdflatex pre-project
-bibtex pre-project
-pdflatex pre-project
-bibtex pre-project
